f2f3d14d7a84db1a59112948d0dea728785f3e5f
Vitor Spiri <vitorspiri1@gmail.com>
2021-08-02 14:39:08 -0300

    Adicionado aula11

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 80
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if(vel > 60){ // Condição simples
+    console.log('VOCÊ FOI MULTADO')
+}
+
+console.log('Use o cinto de segurança sempre')
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'EUA'
+
+if(pais != 'Brasil'){
+    console.log('Você é Estrangeirio')
+}
+else{
+    console.log('Você é Brasileiro')
+}
