84307bc543bd3bba6b92ea9447660527bc1a9a48
Vitor Spiri <vitorspiri1@gmail.com>
2021-08-03 13:48:26 -0300

    Adicionado aula12

---
+++ aula12/ex011.js
@@ -0,0 +1,12 @@
+var idade = 71
+
+if( idade < 16){
+    console.log('Não vota')
+}
+else if ((idade < 18) || (idade >= 70)){
+    console.log('Voto Opcional')   
+}else{
+    console.log('Voto Obrigatório')
+}
+
+
