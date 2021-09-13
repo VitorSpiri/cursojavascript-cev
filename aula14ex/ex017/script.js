b789703af4b773e9d224323afa49655d0e9bc9ea
Vitor Spiri <vitorspiri1@gmail.com>
2021-09-13 13:21:01 -0300

    Adicionado aula14_ex17

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,2 +1,16 @@
-alert('Ola')
-//AINDA NÃO FOI COMMITADO
+function contar(){
+
+    let txtnum = document.getElementById('txtnum')
+    let lista = document.getElementById('lista')
+
+    let n = Number(txtnum.value)
+        
+        for (let c = 1; c <= 10; c++){
+
+            let option = document.createElement('option')
+
+            option.text = ` ${n} x ${c} = ${n*c}`
+
+            lista.appendChild(option)
+        }
+}
